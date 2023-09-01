7b20c1b2caef3c5b5bdc384b1f379ea521c696ae
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2023-09-01 18:25:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const fileInput = document.querySelector("input"),
+downloadBtn = document.querySelector("button");
+
+
+downloadBtn.addEventListener("click", e => {
+    e.preventDefault(); // preventing form from submitting
+    downloadBtn.innerText = "Downloading file...";
+    fetchFile(fileInput.value);
+});
+
+
+function fetchFile(url){
+    // fetching file & returning response as blob
+    fetch(url).then(res => res.blob()).then(file => {
+        //URL .createObjURL create a url of passed object
+        let tempUrl = URL.createObjectURL(file);
+        let aTag  = document.createElement("a");
+        aTag.href = tempUrl; // passing tempUrl as href value of <a> tag
+        // passing file last name & extension as download value of <a> tag
+        aTag.download = url.replace(/.^[\\\/]/, '');
+        document.body.appendChild(aTag); //adding <a> tag inside body
+        aTag.click(); // clicking <a> tag so the file download
+        aTag.remove(); // removing <a> tag once file downloaded
+        URL.revokeObjectURL(tempUrl); //removing the tempUrl from the document
+        downloadBtn.innerText = "Download File"
+    }).catch(() => {
+        // catch method call if any error comes during downloading
+        downloadBtn.innerText = "Download File";
+        alert("Failed to download file!");
+    })
+}
